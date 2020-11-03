853a12dc6c4329e3d4a30a351a2a7f6150887c35
sunnyplace0 <73881525+sunnyplace0@users.noreply.github.com>
2020-11-04 01:23:08 +0900

    Update getUserInfo.js

--- getUserInfo.js
+++ getUserInfo.js
@@ -1,11 +1,11 @@
 $("#btn1").on("click", function()  {
   // jQueryを使って画面にメッセージを表示する
   document.getElementById('eid_href').innerHTML = '現在開いているURL：' + window.location.href; // 現在開いているURLを取得する
-  document.getElementById('eid_search').innerHTML = window.location.search; // URL内のクエリ情報を抽出して取得する
-  document.getElementById('eid_hash').innerHTML = widnow.location.hash; // URL内のハッシュ情報を抽出して取得する
-  document.getElementById('eid_ori').innerHTML = window.location.origin; // プロトコル・ポート情報を含めたURLを取得する
-  document.getElementById('eid_apN').innerHTML = window.navigator.appName; // ブラウザの正式名称を取得する
-  document.getElementById('eid_lang').innerHTML = window.navigator.language; // ブラウザの言語情報を取得する
+  // document.getElementById('eid_search').innerHTML = window.location.search; // URL内のクエリ情報を抽出して取得する
+  // document.getElementById('eid_hash').innerHTML = widnow.location.hash; // URL内のハッシュ情報を抽出して取得する
+  // document.getElementById('eid_ori').innerHTML = window.location.origin; // プロトコル・ポート情報を含めたURLを取得する
+  // document.getElementById('eid_apN').innerHTML = window.navigator.appName; // ブラウザの正式名称を取得する
+  // document.getElementById('eid_lang').innerHTML = window.navigator.language; // ブラウザの言語情報を取得する
   document.getElementById('eid_ua').innerHTML = window.navigator.userAgent; // ブラウザのユーザーエージェントを取得する
   document.getElementById('eid_apV').innerHTML = window.navigator.appVersion; // ブラウザのバージョン情報を取得する
   document.getElementById('eid_ref').innerHTML = window.document.referrer; // 直前に閲覧していたWebページのURLを取得する
