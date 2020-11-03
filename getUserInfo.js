cabac2b4649b2bed64c736153bc1874248d0b75a
sunnyplace0 <73881525+sunnyplace0@users.noreply.github.com>
2020-11-04 01:17:05 +0900

    Update getUserInfo.js

--- getUserInfo.js
+++ getUserInfo.js
@@ -1,6 +1,14 @@
 $("#btn1").on("click", function()  {
   // jQueryを使って画面にメッセージを表示する
-  document.getElementById('eid_useragent').innerHTML = window.navigator.userAgent;
-  document.getElementById('eid_referrer').innerHTML = window.document.referrer;
-  //document.getElementById('eid_ua').innerHTML = window.navigator.userAgent;
+  document.getElementById('eid_href').innerHTML = window.location.href// 現在開いているURLを取得する
+  document.getElementById('eid_search').innerHTML = window.location.search// URL内のクエリ情報を抽出して取得する
+  document.getElementById('eid_hash').innerHTML = widnow.location.hash// URL内のハッシュ情報を抽出して取得する
+  document.getElementById('eid_ori').innerHTML = window.location.origin// プロトコル・ポート情報を含めたURLを取得する
+  document.getElementById('eid_apN').innerHTML = window.navigator.appName// ブラウザの正式名称を取得する
+  document.getElementById('eid_lang').innerHTML = window.navigator.language// ブラウザの言語情報を取得する
+  document.getElementById('eid_ua').innerHTML = window.navigator.userAgent; // ブラウザのユーザーエージェントを取得する
+  document.getElementById('eid_apV').innerHTML = window.navigator.appVersion// ブラウザのバージョン情報を取得する
+  document.getElementById('eid_ref').innerHTML = window.document.referrer; // 直前に閲覧していたWebページのURLを取得する
+  document.getElementById('eid_width').innerHTML = window.screen.width// 画面スクリーンの横幅を取得する
+  document.getElementById('eid_height').innerHTML = window.screen.height// 画面スクリーンの高さを取得する
 });
