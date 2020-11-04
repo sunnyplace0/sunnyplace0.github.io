ab68cc94ab5aff5ecb0114404e6eb95c9b9ecd7d
sunnyplace0 <73881525+sunnyplace0@users.noreply.github.com>
2020-11-04 21:26:52 +0900

    Update getUserInfo.js

--- getUserInfo.js
+++ getUserInfo.js
@@ -3,6 +3,7 @@ $("#btn1").on("click", function()  {
   let data = `JavaScript実行結果`;
   document.getElementById('eid_href').innerHTML = `現在開いているURL：${window.location.href}`;
   document.getElementById('eid_ua').innerHTML = `ブラウザのユーザーエージェント：${window.navigator.userAgent}`;
+  document.getElementById('eid_apV').innerHTML = `ブラウザのバージョン情報：${window.navigator.appVersion}`;
   document.getElementById('eid_width').innerHTML = `画面スクリーンの横幅：${window.screen.width}`;
   document.getElementById('eid_height').innerHTML = `画面スクリーンの高さ：${window.screen.height}`;
   $.get("https://ipinfo.io", function(res) {
@@ -18,8 +19,6 @@ $("#btn1").on("click", function()  {
   document.getElementById('eid_ori').innerHTML = 'プロトコル・ポート情報を含めたURL：' + window.location.origin;
   document.getElementById('eid_apN').innerHTML = 'ブラウザの正式名称：' + window.navigator.appName;
   document.getElementById('eid_lang').innerHTML = 'ブラウザの言語情報：' + window.navigator.language;
-  document.getElementById('eid_ua').innerHTML = 'ブラウザのユーザーエージェント：' + window.navigator.userAgent;
-  document.getElementById('eid_apV').innerHTML = 'ブラウザのバージョン情報：' + window.navigator.appVersion;
   document.getElementById('eid_ref').innerHTML = `直前に閲覧していたWebページのURL：${window.document.referrer}\n`;
   */
 });
