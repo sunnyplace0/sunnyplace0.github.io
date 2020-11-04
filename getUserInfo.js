cd6b1f0f13f0a654adfe0d26a7acbe479d65b12f
sunnyplace0 <73881525+sunnyplace0@users.noreply.github.com>
2020-11-04 19:53:38 +0900

    Update getUserInfo.js

--- getUserInfo.js
+++ getUserInfo.js
@@ -1,13 +1,15 @@
 $("#btn1").on("click", function()  {
   // jQueryを使って画面にメッセージを表示する
-  let data = '現在開いているURL：' + window.location.href;
+  let data = "";
+  '現在開いているURL：' + window.location.href;
   data += 'URL内のクエリ情報：' + window.location.search;
-  //if (!window.location.href) {
-    document.getElementById('eid_href').innerHTML = '現在開いているURL：' + window.location.href;
-  //}
-  //if (!window.location.search) {
-    //document.getElementById('eid_search').innerHTML = 'URL内のクエリ情報：' + window.location.search;
-  //}
+  if (!window.location.href) {
+    data += '現在開いているURL：' + window.location.href;
+  }
+  if (!window.location.search) {
+    data += 'URL内のクエリ情報：' + window.location.search;
+  }
+  document.getElementById('eid_href').innerHTML = data;
   //if (!widnow.location.hash) {
     document.getElementById('eid_hash').innerHTML = 'URL内のハッシュ情報：' + widnow.location.hash;
   //}
